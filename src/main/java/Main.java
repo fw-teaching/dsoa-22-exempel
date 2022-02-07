5471b482b37b0f99008c3b35d51c6e2333bc738f
Fredrik Welander <welandfr@arcada.fi>
2022-02-07 14:50:44 +0200

    mellacomit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,100 +1,34 @@
-import java.util.Arrays;
+import java.util.Scanner;
+
+import javax.sound.sampled.SourceDataLine;
 
 public class Main {
 
+    // Konstant
+    static final double APP_VERSION = 1.0;
+
     public static void main(String[] args) {
 
-        System.out.println("--------  Rekursion -------------");
-        Recursy.hello(5);
 
+        Person[] standardArray = new Person[Utils.TOP_MALE_NAMES.length];
+        Hashy hashy = new Hashy(Utils.TOP_MALE_NAMES.length);
 
+        for (int i = 0; i < standardArray.length; i++) {
 
-        System.out.println("--------  Graf -------------");
-
-        Graphy graphy = new Graphy();
-        graphy.createNode("shi", "Skjorta");
-        graphy.createNode("tie", "Slips");
-        graphy.createNode("jea", "Jeans");
-        graphy.createNode("cro", "Crocs");
-
-        graphy.printGraph();
-
-        graphy.addEdge("shi", "tie");
-        graphy.addEdge("shi", "jea");
-        graphy.addEdge("tie", "shi");
-        graphy.addEdge("tie", "jea");
-        graphy.addEdge("jea", "shi");
-        graphy.addEdge("jea", "tie");
-        graphy.addEdge("jea", "cro");
-        graphy.addEdge("cro", "jea");
-
-        graphy.printGraph();
-
-        System.exit(0);
-
-        System.out.println("-------- Kö (länkad lista)-------------");
-
-        String[] initialArray = { "Fili", "Kili", "Balin", "Dwalin", "Ori", "Nori", "Dori", "Gloin", "Oin", "Bifur", "Bofur", "Bombur", "Thorin" };
-
-        Queuey q = new Queuey();
-        for (int i = 0; i < 5; i++) {
-            q.enqueue(initialArray[i]);
-        }
-        q.dequeue();
-        q.dequeue();
-        q.dequeue();
-
-        System.out.println("-------- Stack -------------");
-        Stacky stacky = new Stacky(8);
-        stacky.push("Fili");
-        stacky.push("Kili");
-        stacky.push("Balin");
-
-        stacky.pop(); // vi tar bort det översta elementet
-        stacky.push("Gimli");
-
-
-
-        System.out.println("-------- Cikulär array -------------");
-        String[] circular = new String[4];
-        circular[0 % 4] = "A";
-        circular[1 % 4] = "B";
-        circular[2 % 4] = "C";
-        circular[3 % 4] = "D";
-        System.out.println(Arrays.toString(circular));
-        circular[4 % 4] = "E";  // 4 % 4 =0
-        circular[5 % 4] = "F";
-        System.out.println(Arrays.toString(circular));
-
-
-
-
-
-        System.out.println("-------- Länkad lista -------------");
-
-        Linky linky = new Linky();
-        linky.add(new Player("Gimli"));
-
-        for (int i = 0; i < 5; i++) {
-            linky.add(new Player(initialArray[i]));
+            standardArray[i] = new Person(Utils.TOP_MALE_NAMES[i], i);
+            hashy.insert(new Person(Utils.TOP_MALE_NAMES[i], i));
         }
 
+        hashy.searchStandarArray("Watson", standardArray);
+        hashy.search("Watson");
+        hashy.searchStandarArray("Liam", standardArray);
+        hashy.search("Liam");
+        hashy.searchStandarArray("Marshall", standardArray);
+        hashy.search("Marshall");
+        hashy.searchStandarArray("Donald", standardArray);
+        hashy.search("Donald");
 
-        System.out.println("-------- Array -------------");
-
-
-        StringArray myArray = new StringArray(initialArray);
-        System.out.println(myArray.toString());
-
-        myArray.add("Gimli");
-        System.out.println(myArray.toString());
-
-        myArray.add(0, "Bilbo");
-        System.out.println(myArray.toString());
-
-        // new BigO();
+        //System.out.println(hashy.hashCode("foo"));
 
     }
-
-
 }
--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,3 +1,24 @@
 public class Person {
+    private String name;
+    private int nameRank;
+    
+    public Person(String name, int nameRank) {
+        this.name = name;
+        this.nameRank = nameRank;
+    }
+
+    public String getRanking() {
+        return name + " is rank " + nameRank;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    
 }
 
